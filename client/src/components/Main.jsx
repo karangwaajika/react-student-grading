83a20bae26165bdfc8533bf2073748b8ce295aea
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-06-25 16:45:21 +0300

    feat: add subjects and students links

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,5 +1,57 @@
-export default function Main(){
-    return(
-        <></>
-    )
+import { Link } from "react-router-dom";
+export default function Main() {
+  return (
+    <main className="main-content">
+      <div className="header">
+        <h1>Tasks to cover</h1>
+      </div>
+      <div className="content">
+        <div className="card">
+          <Link key="card-1" to="">
+            <h3>Add Subject</h3>
+          </Link>
+        </div>
+        <div className="card">
+          <Link key="card-2" to="">
+            <h3>Add Subject</h3>
+          </Link>
+        </div>
+        <div className="card">
+          <Link key="card-3" to="">
+            <h3>Add Subject</h3>
+          </Link>
+        </div>
+        <div className="card">
+          <Link key="card-4" to="">
+            <h3>Add Subject</h3>
+          </Link>
+        </div>
+        <div className="card">
+          <Link key="card-5" to="">
+            <h3>Add Subject</h3>
+          </Link>
+        </div>
+        <div className="card">
+          <Link key="card-6" to="">
+            <h3>Add Subject</h3>
+          </Link>
+        </div>
+        <div className="card">
+          <Link key="card-7" to="">
+            <h3>Add Subject</h3>
+          </Link>
+        </div>
+        <div className="card">
+          <Link key="card-8" to="">
+            <h3>Add Subject</h3>
+          </Link>
+        </div>
+        <div className="card">
+          <Link key="card-9" to="">
+            <h3>Add Subject</h3>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
