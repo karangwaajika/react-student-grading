f48f8b521786e700177d5931e26e93eee0e5b0ed
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-06-26 11:31:53 +0300

    feat: connect mongodb

--- server/app.mjs
+++ server/app.mjs
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
+
+mongoose.connect(process.env.MongoStringID).then(()=>{
+  console.log("database connected")
+}).catch(err=>console.log(err))
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello world");
