4da661572cd1218170738f04f9172b8a7abd966e
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-07-03 16:27:19 +0300

    feat: log the user in using jwt

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,49 +1,71 @@
 import { useState } from "react";
 import axios from "axios";
-import {Link, Navigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import FlashMessage from './FlashMessage'
 export default function Login() {
   const[form, setForm] = useState({
     email:"",
     password: "",
   })
 
+  const [responseMessage, setResponseMessage] = useState();
+  const removeMessage = () => {
+    setResponseMessage();
+  };
+
+  const navigate = useNavigate()
+
   const handleChange = (e) =>{
      const {name, value} = e.target
      setForm(prevForm =>{
       return {...prevForm, [name]: value}
      })
-     console.log(form)
+     
   }
 
   const handleSubmit = async (e) =>{
     e.preventDefault()
-    const formData = new FormData()
-    formData.append('email', form.email)
-    formData.append('password', form.password)
-    
-    // axios.post('http://localhost:3001/login_user', formData)
-    // .then(res=>console.log(res))
-    // .catch(err=>console.log(err))
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email: form.email, password:form.password })
-  };
-  try{
-    const response = await fetch('http://localhost:3001/login_user', requestOptions);
-    const data = await response.json();
-    console.log(data)
-  }catch(err){
-    console.log("failed")
-    console.log(err)
-  }
-  
-  
+
+    axios.post('http://localhost:3001/login_user', { email: form.email, password:form.password })
+    .then(res=>{
+      if(res.data.success){
+        localStorage.setItem('token', res.data.token) //store the token in local storage
+        navigate('/dashboard')
+      }
+          })
+    .catch(err=>{
+      console.log(err.response.data)
+      setResponseMessage({
+        success: false,
+        message: err.response.data.message,
+      });
+    })
+
+  //   const requestOptions = {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({ email: form.email, password:form.password })
+  // };
+  // try{
+  //   const response = await fetch('http://localhost:3001/login_user', requestOptions);
+  //   const data = await response.json();
+  //   console.log(data)
+  // }catch(err){
+  //   console.log("failed")
+  //   console.log(err)
+  // }
     }
 
   return (
     <>
       <h3>Sign-in</h3>
+      {responseMessage && (
+          <FlashMessage
+            message={responseMessage.message}
+            success={responseMessage.success}
+            removeMessage={removeMessage}
+          />
+        )}
       <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="email-login">Email</label>
