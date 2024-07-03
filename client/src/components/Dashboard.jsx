b071f6b74688f9c768d5a5e146f5d3da9ef0b912
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-07-03 16:57:05 +0300

    refactor: refactor and restructure codes

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,35 +1,37 @@
-import axios from 'axios'
-import '../assets/dashboard.css'
-import Main from './Main'
-import Nav from './Nav'
-import Profile from './Profile'
-import SideBar from './SideBar'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import "../assets/dashboard.css";
+import Main from "./Main";
+import Nav from "./Nav";
+import Profile from "./Profile";
+import SideBar from "./SideBar";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Dashboard(){
-    const navigate = useNavigate();
-    useEffect(()=>{
-        const token = localStorage.getItem('token')
-        axios.get('http://localhost:3001/protected', {
-            headers:{
-                Authorization: token
-            }
-        }).then().catch(err=>{
-            navigate('/')
-        })
-
-    }, [])
-    return (
-        <div className="dashboard-container">
-            <aside className='left-side'>
-                <Profile />
-                <SideBar />
-            </aside>
-            <aside className='right-side'>
-                <Nav />
-                <Main />
-            </aside>
-        </div>
-    )
+export default function Dashboard() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get("http://localhost:3001/protected", {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then()
+      .catch((err) => {
+        navigate("/");
+      });
+  }, []);
+  return (
+    <div className="dashboard-container">
+      <aside className="left-side">
+        <Profile />
+        <SideBar />
+      </aside>
+      <aside className="right-side">
+        <Nav />
+        <Main />
+      </aside>
+    </div>
+  );
 }
