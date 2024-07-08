5734ce07e4c15afa2bd32080c3a832d3ad979f1e
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-07-08 12:35:23 +0300

    feat: log a user out

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,33 +1,56 @@
-import {Link, Outlet} from 'react-router-dom'
-import { UserContent } from "../pages/Dashboard"
-import { useContext } from "react"
+import { Link, Outlet } from "react-router-dom";
+import { UserContent } from "../pages/Dashboard";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Nav(){
-    const user = useContext(UserContent)
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay = ''
-    if(hours < 12){
-        timeOfDay = "Good morning"
-    }else if(hours >= 12 && hours < 18){
-        timeOfDay = "Good afternoon"
-    }else{
-        timeOfDay = "Good evening"
-    }
-    return (
-        <nav>
-            <p><b>{timeOfDay}</b>! Mr {user.fullname && user.fullname}</p>
-            <ul className="links">
-                <li className="link"> <Link key="link1" to="/dashboard">Dashboard</Link></li>
-                <li className="link"><Link key="link1" to="/dashboard">Students</Link></li>
-                <li className="link"><Link key="link1" to="/dashboard">Grades</Link></li>
-            </ul>
-            <div className="profile">
-                <div className="name">
-                    <h5>{user.email && user.email}</h5>
-                </div>
-                <Link key="out" to="/dashboard"><i className='fa fa-sign-out'></i></Link>
-            </div>
-        </nav>
-    )
+export default function Nav() {
+  const user = useContext(UserContent);
+  const navigate = useNavigate();
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay = "";
+  if (hours < 12) {
+    timeOfDay = "Good morning";
+  } else if (hours >= 12 && hours < 18) {
+    timeOfDay = "Good afternoon";
+  } else {
+    timeOfDay = "Good evening";
+  }
+  const logout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+  return (
+    <nav>
+      <p>
+        <b>{timeOfDay}</b>! Mr {user.fullname && user.fullname}
+      </p>
+      <ul className="links">
+        <li className="link">
+          {" "}
+          <Link key="link1" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li className="link">
+          <Link key="link1" to="/dashboard">
+            Students
+          </Link>
+        </li>
+        <li className="link">
+          <Link key="link1" to="/dashboard">
+            Grades
+          </Link>
+        </li>
+      </ul>
+      <div className="profile">
+        <div className="name">
+          <h5>{user.email && user.email}</h5>
+        </div>
+        <p key="out" onClick={logout}>
+          <i className="fa fa-sign-out"></i>
+        </p>
+      </div>
+    </nav>
+  );
 }
