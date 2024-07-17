2e01b125da9de438039994d0b3f09260e216bf45
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-07-17 09:58:28 +0300

    style: format and add semicolon

--- client/src/components/StudentTable.jsx
+++ client/src/components/StudentTable.jsx
@@ -1,62 +1,63 @@
 export default function StudentTable({
-    students,
-    getRowToEditIndex,
-    getRowToDeleteIndex,
-  }) {
-    return (
-      <>
-        <table>
-          <caption>Student List</caption>
-          <thead>
+  students,
+  getRowToEditIndex,
+  getRowToDeleteIndex,
+}) {
+  return (
+    <>
+      <table>
+        <caption>Student List</caption>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Roll Number</th>
+            <th>Academic Year</th>
+            <th>Subject</th>
+            <th>Date Created</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.length > 0 ? (
+            students.map((student, index) => {
+              return (
+                <tr key={index + 1}>
+                  <td data-cell="nbr">{index + 1}</td>
+                  <td data-cell="name">{student.name}</td>
+                  <td data-cell="code">{student.code}</td>
+                  <td data-cell="academicYear">{student.academicYear}</td>
+                  <td data-cell="favoriteSubject">
+                    {student.favoriteSubject.name}
+                  </td>
+                  <td data-cell="date">{student.createdAt}</td>
+                  <td data-cell="action">
+                    <i
+                      className="fa fa-pencil"
+                      onClick={() => {
+                        getRowToEditIndex(index);
+                      }}
+                    ></i>{" "}
+                    <i
+                      className="fa fa-trash-can"
+                      style={{ color: "red" }}
+                      onClick={() => {
+                        getRowToDeleteIndex(index);
+                      }}
+                    ></i>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
             <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Roll Number</th>
-              <th>Academic Year</th>
-              <th>Subject</th>
-              <th>Date Created</th>
-              <th>Action</th>
+              <td colSpan={7} style={{ textAlign: "center", color: "red" }}>
+                No data
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {students.length > 0 ? (
-              students.map((student, index) => {
-                return (
-                  <tr key={index + 1}>
-                    <td data-cell="nbr">{index + 1}</td>
-                    <td data-cell="name">{student.name}</td>
-                    <td data-cell="code">{student.code}</td>
-                    <td data-cell="academicYear">{student.academicYear}</td>
-                    <td data-cell="favoriteSubject">{student.favoriteSubject.name}</td>
-                    <td data-cell="date">{student.createdAt}</td>
-                    <td data-cell="action">
-                      <i
-                        className="fa fa-pencil"
-                        onClick={() => {
-                            getRowToEditIndex(index);
-                        }}
-                      ></i>{" "}
-                      <i
-                        className="fa fa-trash-can"
-                        style={{ color: "red" }}
-                        onClick={() => {
-                          getRowToDeleteIndex(index);
-                        }}
-                      ></i>
-                    </td>
-                  </tr>
-                );
-              })
-            ) : (
-              <tr>
-                <td colSpan={7} style={{ textAlign: "center", color: "red" }}>
-                  No data
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </>
-    );
-  }
-  
+          )}
+        </tbody>
+      </table>
+    </>
+  );
+}
--- server/route/studentRoute.mjs
+++ server/route/studentRoute.mjs
@@ -6,6 +6,6 @@ const route = express();
 route.post("/add_student", controller.addStudent);
 route.get("/view_students", controller.viewStudents);
 route.post("/update_student", controller.updateStudent);
-route.post('/delete_student', controller.deleteStudent)
+route.post("/delete_student", controller.deleteStudent);
 
 export default route;
