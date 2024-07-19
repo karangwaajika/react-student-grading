f160b0e43ddb2f07225030f603f49a353c1292f0
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-07-19 11:09:09 +0300

    fix: prevent marks insertion from duplicating.
    Marks of a subject are inserted once at a given trimester of a specific year.
    These changes are made based on this approach to prevent duplication.

--- server/controllers/marksController.mjs
+++ server/controllers/marksController.mjs
@@ -5,6 +5,7 @@ export const addMarks = (request, response) => {
   const {
     body: { subjectId, studentCode, marks, trimester },
   } = request;
+  // search student by studentCode in order to get the ID
   Student.findOne({ code: studentCode })
     .then((student) => {
       if (student) {
@@ -14,19 +15,57 @@ export const addMarks = (request, response) => {
           trimester,
           studentId: student._id,
         });
-        Marks.create(newMarks)
-          .then(() => {
-            return response.send({
-              success: true,
-              message: "Marks inserted successfully",
+        // check if student has marks to avoid duplication before inserting
+        Marks.find({ studentId: student._id }).then((studentMarksList) => {
+          if (studentMarksList.length > 0) {
+            studentMarksList.forEach((studentMarks) => {
+              const nowDate = new Date();
+              const nowYear = nowDate.getFullYear();
+              const markingDate = new Date(studentMarks.createdAt);
+              const markingYear = markingDate.getFullYear();
+
+              if (
+                studentMarks.subjectId == subjectId &&
+                markingYear == nowYear && studentMarks.trimester == trimester
+              ) {
+                return response.send({
+                  success: false,
+                  message: `Marks already inserted for trimester ${trimester} of this year`,
+                });
+              } else {
+                // insert marks
+                Marks.create(newMarks)
+                  .then(() => {
+                    return response.send({
+                      success: true,
+                      message: "Marks inserted successfully",
+                    });
+                  })
+                  .catch((err) => {
+                    return response.send({
+                      success: false,
+                      message: "Error encouted, try again.",
+                    });
+                  });
+              }
             });
-          })
-          .catch((err) => {
-            return response.send({
-              success: false,
-              message: "Error encouted, try again.",
-            });
-          });
+          } else {
+            // insert marks as student has none
+            Marks.create(newMarks)
+              .then(() => {
+                return response.send({
+                  success: true,
+                  message: "Marks inserted successfully",
+                });
+              })
+              .catch((err) => {
+                return response.send({
+                  success: false,
+                  message: "Error encouted, try again.",
+                });
+              });
+          }
+        });
       } else {
         return response.send({
           success: false,
