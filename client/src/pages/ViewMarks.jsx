a530815021f5a89800c0f2ce2cd41c3cd9e6b397
ajika <52496651+karangwaajika@users.noreply.github.com>
2024-07-22 15:20:35 +0300

    feat: add loading image while fetching marks

--- client/src/pages/ViewMarks.jsx
+++ client/src/pages/ViewMarks.jsx
@@ -1,6 +1,7 @@
 import useFetchMarks from "../Hooks/useFetchMarks";
 import MarksTable from "../components/MarksTable";
 import FlashMessage from "../components/ui/FlashMessage";
+import loadingPicture from "/images/giphy-1.webp";
 
 export default function ViewMarks() {
   const { marks, isLoading, responseMessage, removeMessage } = useFetchMarks();
@@ -13,7 +14,11 @@ export default function ViewMarks() {
           removeMessage={removeMessage}
         />
       )}
-
+      {isLoading && (
+        <div className="loader">
+          <img src={loadingPicture} width={100} height={100} />
+        </div>
+      )}
       <MarksTable marks={marks} />
     </div>
   );
